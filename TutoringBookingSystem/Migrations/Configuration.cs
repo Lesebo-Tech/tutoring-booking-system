d4c464b30c7c095dcfbba2093859e91c03ad22d2
Lesebo <owamixaba01@gmail.com>
2026-08-17 09:24:17 +0200

    Remove sensitive credentials from source control, add config template

--- TutoringBookingSystem/Migrations/Configuration.cs
+++ TutoringBookingSystem/Migrations/Configuration.cs
@@ -14,16 +14,7 @@
 
         protected override void Seed(TutoringBookingSystem.Models.TutoringContext context)
         {
-            if (!context.Tutors.Any())
-            {
-                context.Tutors.Add(new TutoringBookingSystem.Models.Tutor
-                {
-                    Username = "lesebo",
-                    PasswordHash = System.Web.Helpers.Crypto.HashPassword("25$Andisa"),
-                    Email = "owamixaba01@gmail.com"
-                });
-                context.SaveChanges();
-            }
+           
         }
     }
 }
